db17e51163cc8cf0ecb53514a1f8819a1b011dfc
Marco Primo <up201800388@fc.up.pt>
2022-03-24 08:07:22 +0000

    new

---
+++ src/Main.js
@@ -0,0 +1,11 @@
+import './Main.css'
+import particlesIMG from './particlesIMG'
+
+function InitialScreen() {
+  return (
+    <canvas id="CANVAS">
+    </canvas>
+  )
+}
+
+export default InitialScreen
